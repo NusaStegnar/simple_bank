f457e01a9a92ad745086daa836dbcbef51a79599
sanusa644 <sanusa644@gmail.com>
2024-03-26 11:55:06 +0100

    readme file

--- test.py
+++
@@ -1,8 +0,0 @@
-
-def get_loan_monthly_repayment(amt, months, interest):
-    monthly_rate = interest/12
-    monthly_repayment = amt*((monthly_rate*((monthly_rate + 1)**months)/(((monthly_rate + 1)**months) - 1)))
-    return round(monthly_repayment)
-
-
-print(get_loan_monthly_repayment(50000, 48, 0.06))
